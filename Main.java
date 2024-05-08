020888c57bafa9f32c875235fe5fb6e27a3de360
kivancterzioglu <kivancter2@gmail.com>
2024-05-08 16:51:03 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,52 @@
+package HashTable;
+
+public class Main {
+    public static void main(String[] args) {
+        /* 
+        HashTable myHashTable = new HashTable();
+
+        // HASH METHOD MUST BE SET TO PUBLIC FOR THESE LINES TO WORK
+        System.out.println( myHashTable.hash("paint") );
+        System.out.println( myHashTable.hash("bolts") );
+        System.out.println( myHashTable.hash("nails") );
+        System.out.println( myHashTable.hash("stuff") );
+        System.out.println( myHashTable.hash("lumber") );*/
+
+
+      /*   HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
+        myHashTable.printTable();*/
+/* 
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+
+        System.out.println("Lumber:");
+        System.out.println( myHashTable.get("lumber") );
+
+        System.out.println("\nBolts:");
+        System.out.println( myHashTable.get("bolts") );*/
+
+
+
+
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("paint", 20);
+        myHashTable.set("bolts", 40);
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+
+        System.out.println( myHashTable.keys() );
+
+    }
+}
